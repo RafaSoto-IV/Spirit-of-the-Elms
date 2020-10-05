dcf96804db68c683c24b561202cd524675429f1a
Tyler <Tyler@MacBook-Air-62.local>
2020-10-05 16:51:34 -0500

    add cropping for heatlh bar

--- uiScene.js
+++ uiScene.js
@@ -5,31 +5,20 @@ class uiScene extends Phaser.Scene {
 
     }
 
-    // preload(){
-    //   this.load.image('healthbar', 'healthbar.png');
-    //   this.load.image('manabar', 'manabar.png');
-    // }
-
-
     create(){
         //  Grab a reference to the Game Scene
         let ourGame = this.scene.get('playGame');
+
         //  health and mana bars
         console.log(ourGame.player);
-        this.healthbar = this.add.image(66, 58, 'healthbar');
-        this.manabar = this.add.image(66, 60, 'manabar');
+        this.healthbar = this.add.image(66, 10, 'healthbar');
+        this.manabar = this.add.image(66, 20, 'manabar');
         this.healthbarMaxWidth = this.healthbar.width;
-        // this.healthbar = this.add.image(ourGame.player.x - 130, ourGame.player.y - 75, 'healthbar');
-        // this.manabar = this.add.image(ourGame.player.x - 130, ourGame.player.y - 73, 'manabar');
-        console.log(this.healthbar);
-        // this.healthbar.setScrollFactor(0);
-        // this.manabar.setScrollFactor(0);
+        var check = true;
         ourGame.events.on('playerHit', function () {
-           //console.log("Player hit");
-           console.log(this.healthbar.width)
-           console.log(this.healthbar.height)
-           this.healthbar.displayWidth = 100;
-           //this.healthbar.setDisplaySize(100, this.healthbar.height)
+           //var rect = new Phaser.Geom.Rectangle(0, 0, 20 , this.healthbar.height);
+           this.healthbar.setCrop(0, 0, this.healthbar.width/2 , this.healthbar.height);
+
        }, this);
 
 
