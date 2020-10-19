fba408c5b5489481f3195d98c30a4093764b17cf
Tyler <Tyler@MacBook-Air-62.local>
2020-10-19 15:21:36 -0500

    add level up for projectile damage

--- LevelUpScreenScene.js
+++ LevelUpScreenScene.js
@@ -12,11 +12,14 @@ class LevelUpScreenScene extends Phaser.Scene {
         const instructionText = this.add.text(20, 80, "Click on the following text to increase your health or mana.", { fontFamily: "Verdana", fontSize: '12px', fill: '#FFF' }).setScale( 1 / this.cameras.main.zoom, 1 / this.cameras.main.zoom );
         const healthText = this.add.text(150, 110, "Increase Health", { fontFamily: "Verdana", fontSize: '12px', fill: '#FFF' }).setScale( 1 / this.cameras.main.zoom, 1 / this.cameras.main.zoom );
         const manaText = this.add.text(150, 140, "Increase Mana", { fontFamily: "Verdana", fontSize: '12px', fill: '#FFF' }).setScale( 1 / this.cameras.main.zoom, 1 / this.cameras.main.zoom );
+        const projectileDamageText = this.add.text(115, 170, "Increase Projectile Damage", { fontFamily: "Verdana", fontSize: '12px', fill: '#FFF' }).setScale( 1 / this.cameras.main.zoom, 1 / this.cameras.main.zoom );
 
         manaText.setInteractive(new Phaser.Geom.Rectangle(0, 0, manaText.width, manaText.height), Phaser.Geom.Rectangle.Contains);
         manaText.on('pointerdown', this.levelUpMana, this);
         healthText.setInteractive(new Phaser.Geom.Rectangle(0, 0, healthText.width, healthText.height), Phaser.Geom.Rectangle.Contains);
         healthText.on('pointerdown', this.levelUpHealth, this);
+        projectileDamageText.setInteractive(new Phaser.Geom.Rectangle(0, 0, projectileDamageText.width, projectileDamageText.height), Phaser.Geom.Rectangle.Contains);
+        projectileDamageText.on('pointerdown', this.levelUpProjectileDamage, this);
 
 
     }
@@ -32,6 +35,11 @@ class LevelUpScreenScene extends Phaser.Scene {
       this.ourGame.player.maxHealth += 200;
       this.ourGame.player.health = this.ourGame.player.maxHealth;
       this.ourGame.events.emit('playerHit');
+      this.unPause();
+    }
+
+    levelUpProjectileDamage(){
+      this.ourGame.player.projectileDamage += 100;
       this.unPause();
     }
 
